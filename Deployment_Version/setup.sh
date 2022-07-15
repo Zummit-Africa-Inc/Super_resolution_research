13cdfe9b21847ea83fa6e922f7a128b86f3b3fba
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-15 17:29:19 +0100

    updated shell script

--- Deployment_Version/setup.sh
+++ Deployment_Version/setup.sh
@@ -1,6 +1,13 @@
+# Update and install requirements
+sudo apt-get update
+sudo apt install -y python3-pip nginx
+sudo apt install -y uvicorn
+# Copy the configuration file to the nginx enabled sites folder
+sudo cp -R fastapi_setup /etc/nginx/sites-enabled/
+sudo service nginx restart
 sudo git clone https://github.com/Nneji123/DeOldify.git
 cd DeOldify
-sudo pip install -r requirements.txt
+sudo pip3 install -r requirements.txt
 sudo wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 sudo tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 sudo ngrok authtoken 29vr8YhWZ9CDHrUq2kr0CpUA0e8_6ik4hU5GjJZeAYagTH5i4
@@ -13,3 +20,4 @@ sudo wget -O /root/.torch/models/resnet34-333f7ec4.pth https://download.pytorch.
 
 sudo wget -O ~/DeOldify/models/ColorizeArtistic_gen.pth https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth
 sudo wget -O ~/DeOldify/models/ColorizeVideo_gen.pth https://data.deepai.org/deoldify/ColorizeVideo_gen.pth
+cp -R ~/Super_resolution_research/app.py ~/DeOldify 
