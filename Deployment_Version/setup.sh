35f7ce6c9ccd321f9869287057b87381681c7a7d
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-17 13:33:40 +0100

    update dockerfile

--- Deployment_Version/setup.sh
+++ Deployment_Version/setup.sh
@@ -1,12 +1,3 @@
-#!/bin/sh
-git clone https://github.com/Nneji123/DeOldify.git ~/DeOldify
-cd ~/DeOldify
-pip3 install -r requirements.txt
-mkdir -p /root/.torch/models
-mkdir -p ~/DeOldify/models
-
-wget -O /root/.torch/models/vgg16_bn-6c64b313.pth https://download.pytorch.org/models/vgg16_bn-6c64b313.pth
-
-wget -O /root/.torch/models/resnet34-333f7ec4.pth https://download.pytorch.org/models/resnet34-333f7ec4.pth
-
-
+mkdir -p /root/.torch/models  
+wget -O /root/.torch/models/ vgg16_bn-6c64b313.pth https://download.pytorch.org/models/vgg16_bn-6c64b313.pth 
+wget -O /root/.torch/models/resnet34-333f7ec4.pth https://download.pytorch.org/models/resnet34-333f7ec4.pth 
