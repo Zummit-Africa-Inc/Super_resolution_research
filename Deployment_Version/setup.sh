1781a227c892071be43e138ea4c2b97d21e2d716
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-17 19:23:48 +0100

    deleted redundant test files

--- Deployment_Version/setup.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p /root/.torch/models  
-wget -O /root/.torch/models/ vgg16_bn-6c64b313.pth https://download.pytorch.org/models/vgg16_bn-6c64b313.pth 
-wget -O /root/.torch/models/resnet34-333f7ec4.pth https://download.pytorch.org/models/resnet34-333f7ec4.pth 
